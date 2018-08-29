d33387fe31d259794f2f1c0daec0635bf3df777a
iamshf <haofengsheng@gmail.com>
2018-08-29 21:48:12 +0800

    add post manage page

---
+++ Controllers/Admin/PostController.php
@@ -0,0 +1,18 @@
+<?php
+namespace iWriter\Controllers\Admin {
+    use iWriter\Controllers\Admin\Resource;
+    use iWriter\Models\Admin\PostModel;
+    class PostController extends Resource {
+        public function getHtml() {
+            $file = '../Views/Admin/Post.html';
+            $expire = \Conf::CACHE_EXPIRE * 86400 ;//缓存一天
+            $this->_headers[] = 'Content-Type: text/html;charset=UTF-8';
+
+            $this->_headers[] = 'Expires: ' . gmdate("D, d M Y H:i:s", time() + $expire) . ' GMT';
+            $this->_headers[] = 'Cache-Control: max-age=' . $expire;
+
+            $this->_headers[] = 'Last-Modified: ' . gmdate("D, d M Y H:i:s", filemtime($file)) . ' GMT';
+            $this->_body = $this->render($file);
+        }
+    }
+}
