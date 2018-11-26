e5046aa5dc1a4e4e1f63409ce40d2332254ede81
iamshf <haofengsheng@gmail.com>
2018-11-26 10:13:50 +0800

    update header info: last modify time

--- Controllers/IndexController.php
+++ Controllers/IndexController.php
@@ -6,14 +6,15 @@ namespace iWriter\Controllers {
     class IndexController extends Resource {
         public function getHtml() {
             $file = '../Views/Index.html';
-            $expire = \Conf::CACHE_EXPIRE * 86400 ;//缓存一天
-            $this->_headers[] = 'Content-Type: text/html;charset=UTF-8';
+            $expire = \Conf::CACHE_EXPIRE;
+            $model = new IndexModel($this->_request->_data);
+            $views = $model->getViews();
 
+            $this->_headers[] = 'Content-Type: text/html;charset=UTF-8';
             $this->_headers[] = 'Expires: ' . gmdate("D, d M Y H:i:s", time() + $expire) . ' GMT';
             $this->_headers[] = 'Cache-Control: max-age=' . $expire;
+            $this->_headers[] = 'Last-Modified: ' . gmdate("D, d M Y H:i:s", (!empty($views['posts']) ? strtotime($views['posts'][0]['gmt_modify']) : filemtime($file))) . ' GMT';
 
-            $this->_headers[] = 'Last-Modified: ' . gmdate("D, d M Y H:i:s", filemtime($file)) . ' GMT';
-            $model = new IndexModel($this->_request->_data);
             $this->_body = $this->render($file, $model->getViews());
         }
         public function getJson() {
--- Controllers/PostController.php
+++ Controllers/PostController.php
@@ -5,15 +5,15 @@ namespace iWriter\Controllers {
     class PostController extends Resource {
         public function getHtml() {
             $file = '../Views/Post.html';
-            $expire = \Conf::CACHE_EXPIRE * 86400 ;//缓存一天
-            $this->_headers[] = 'Content-Type: text/html;charset=UTF-8';
-
-            $this->_headers[] = 'Expires: ' . gmdate("D, d M Y H:i:s", time() + $expire) . ' GMT';
-            $this->_headers[] = 'Cache-Control: max-age=' . $expire;
-
-            $this->_headers[] = 'Last-Modified: ' . gmdate("D, d M Y H:i:s", filemtime($file)) . ' GMT';
+            $expire = \Conf::CACHE_EXPIRE * 30 ;//缓存一天
             $model = new PostModel($this->_request->_data);
             $views = $model->getViews();
+
+            $this->_headers[] = 'Content-Type: text/html;charset=UTF-8';
+            $this->_headers[] = 'Expires: ' . gmdate("D, d M Y H:i:s", time() + $expire) . ' GMT';
+            $this->_headers[] = 'Cache-Control: max-age=' . $expire;
+            $this->_headers[] = 'Last-Modified: ' . gmdate("D, d M Y H:i:s", (!empty($views) ? strtotime($views['gmt_modify']) : filemtime($file))) . ' GMT';
+
             $this->_body = $this->render($file, $views);
         }
     }
